60ffb21b8522f9fb04ef5240ad4e93a0531f139b
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2018-01-31 10:13:15 -0500

    First Upload
    Pug Templates

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, (err) => {
+    if(err){
+        return console.log('Server Error');
+        process.exit(1);
+    }
+    console.log('Server listening on port 3000 ...');
+});
