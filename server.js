cd9c0b561b3b36b9b2eba92c1147a3427e3ca566
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2018-03-05 16:50:50 -0500

    258 SuperAgent Requests
    Request with superAgent Library

--- server.js
+++ server.js
@@ -15,6 +15,46 @@ app.get('/signin', (req, res) => {
     res.render('index', { title : 'Instagram - Signin' });
 });
 
+// middlewares
+app.get('/api/pictures', function(req, res){
+    var pictures = [
+        {
+            user : {
+                username : 'Ernesto Arbelaez', 
+                avatar : 'https://www.shareicon.net/data/128x128/2016/08/18/810199_user_512x512.png'
+            }, 
+            url : 'http://materializecss.com/images/sample-1.jpg',
+            likes : 3,
+            liked : true, 
+            createAt : new Date().getTime()
+        },
+        {
+            user : {
+                username : 'Juan Pablo M.', 
+                avatar : 'https://www.shareicon.net/data/128x128/2016/08/18/810199_user_512x512.png'
+            }, 
+            url : 'https://www.shareicon.net/data/256x256/2015/12/07/683840_image_512x512.png',
+            likes: 4,
+            liked : false,
+            createAt : new Date().setDate(0)
+        },
+        {
+            user : {
+                username : 'Camilo Montoya', 
+                avatar : 'https://www.shareicon.net/data/128x128/2016/08/18/810199_user_512x512.png'
+            }, 
+            url : 'https://www.shareicon.net/data/256x256/2015/12/07/683840_image_512x512.png',
+            likes: 0,
+            liked : false,
+            createAt : new Date().setDate(-10)
+        }
+    ];
+    //simular acceso a BD
+    setTimeout(() => {
+        res.send(pictures); 
+    }, 2000);   
+})
+
 app.listen(3000, (err) => {
     if(err){
         return console.log('Server Error');
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,41 +2,23 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header = require('../header');
 
-page('/', (ctx, next)=> {
+page('/', header, loadPictures,(ctx, next)=> {
     title('Instagram');
     var main = document.getElementById('main-container');   
-    var pictures = [
-        {
-            user : {
-                username : 'Ernesto Arbelaez', 
-                avatar : 'https://www.shareicon.net/data/128x128/2016/08/18/810199_user_512x512.png'
-            }, 
-            url : 'http://materializecss.com/images/sample-1.jpg',
-            likes : 3,
-            liked : true, 
-            createAt : new Date()
-        },
-        {
-            user : {
-                username : 'Juan Pablo M.', 
-                avatar : 'https://www.shareicon.net/data/128x128/2016/08/18/810199_user_512x512.png'
-            }, 
-            url : 'https://www.shareicon.net/data/256x256/2015/12/07/683840_image_512x512.png',
-            likes: 4,
-            liked : false,
-            createAt : new Date().setDate(0)
-        },
-        {
-            user : {
-                username : 'Camilo Montoya', 
-                avatar : 'https://www.shareicon.net/data/128x128/2016/08/18/810199_user_512x512.png'
-            }, 
-            url : 'https://www.shareicon.net/data/256x256/2015/12/07/683840_image_512x512.png',
-            likes: 0,
-            liked : false,
-            createAt : new Date().setDate(-10)
-        }
-    ];
-    empty(main).appendChild(template(pictures));  
+    empty(main).appendChild(template(ctx.pictures));  
 });
+
+function loadPictures(ctx, next){
+    request
+        .get('/api/pictures')
+        .end(function(err, res){
+            if(err){
+                return console.log(err);
+            }
+            ctx.pictures = res.body;
+            next();
+        })
+}
